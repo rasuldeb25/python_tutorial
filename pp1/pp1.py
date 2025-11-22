2fd0f075b965ba8ee894a544679539bd87d74ee2
Rasulbek_Mashalov <rasulbekmashalov@mailbox.unideb.hu>
2025-11-22 22:46:50 +0100

    Initial commit

---
+++ pp1/pp1.py
@@ -0,0 +1,38 @@
+#comment here
+#print("I like plov")
+#print('It tastes good')
+#strings
+from operator import truediv
+
+first_name = "My name"
+food = "plov"
+email = "whoknows@mail.com"
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+#integers
+age = 25
+quantity = 3
+numOfStudents = 30
+print(f"I am {age} years old")
+print(f"You are buying {quantity} of food")
+print(f"There are {numOfStudents} students in the class")
+#floats
+price = 10.99
+gpa = 4.4
+distance = 5.5
+print(f"The price is ${price}")
+print(f"The gpa is {gpa}")
+print(f"The distance is {distance}km")
+#boolean
+isStudent = False
+forSale = False
+print(f"are you a student? {isStudent}")
+if isStudent:
+    print("Yes")
+else:
+    print("No")
+if forSale:
+    print("Yes")
+else:
+    print("No")
