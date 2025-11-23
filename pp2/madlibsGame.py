2c50010eb70feb07c6a8d58b40abc5a8e794450b
Rasulbek_Mashalov <rasulbekmashalov@mailbox.unideb.hu>
2025-11-24 00:53:41 +0100

    madlibsGame added

---
+++ pp2/madlibsGame.py
@@ -0,0 +1,14 @@
+#MadlibsGame
+#Word game where you create a story
+#by filling the blanks with a random words
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
+verb1 = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+
+print(f"Today I went to a {adjective1} zoo. ")
+print(f"In an exhibit I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
