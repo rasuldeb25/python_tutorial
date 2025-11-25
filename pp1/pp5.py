eb691a95b49322493dbbfe33a5f45628cfec452a
Rasulbek_Mashalov <rasulbekmashalov@mailbox.unideb.hu>
2025-11-26 00:03:10 +0100

    pp5 is added

---
+++ pp1/pp5.py
@@ -0,0 +1,39 @@
+#if = Do some code only if some condition is true
+#else = Do something else
+
+"""
+age = int(input('Enter your age: '))
+
+if age >= 100:
+    print('You are a century old!')
+elif age >=18:
+    print('You are an adult')
+elif age < 0:
+    print('You have not been born yet!')
+else:
+    print('You are not an adult')
+"""
+
+"""
+response = input("Would you like a food?(yes/no): ")
+if response == 'yes':
+    print('Here is your food')
+else:
+    print('No food is selected')
+"""
+
+"""
+name  = input("Enter your name: ")
+
+if name == "":
+    print("You didn't enter a name")
+else:
+    print(f"Hello {name}!")
+"""
+
+forSale = False
+
+if forSale:
+    print("Yes, this item is for sale")
+else:
+    print("No, this item is not for a sale")
