84b8b49acd7b611a5aad513f17a998e491fb7d37
Rasulbek_Mashalov <rasulbekmashalov@mailbox.unideb.hu>
2025-12-10 14:28:21 +0100

    pp17 updated

--- pp1/pp17.py
+++ pp1/pp17.py
@@ -1,3 +1,13 @@
 #nested loop = a loop inside a loop (outer, inner)
 #               outer loop
 #               inner loop
+
+
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+symbol = input("Enter a symbol to use: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end=" ")
+    print()
