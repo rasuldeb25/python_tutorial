6d01851532e2c03577587033ea5e124cdb95e602
Rasulbek_Mashalov <rasulbekmashalov@mailbox.unideb.hu>
2026-01-17 09:04:00 +0100

    pp45 added

---
+++ pp1/pp45.py
@@ -0,0 +1,24 @@
+#Decorator  = a function that extends the behaviour of another function
+#               w/o modifying the base function
+#               pass the base function as  an argument  to the decorator
+
+#               @add_sprinkles
+#               get_icecream("vanilla")
+
+def add_sprinkles(func):
+    def wrapper(*args, **kwargs):
+        print("adding sprinkles🧁...")
+        func(*args, **kwargs)
+    return wrapper
+def add_fudge(func):
+    def wrapper(*args, **kwargs):
+        print("fudge is being added🍫...")
+        func(*args, **kwargs)
+    return wrapper
+
+@add_sprinkles
+@add_fudge
+def get_ice_cream(flavor):
+    print(f"Here is your {flavor} icecream🍧")
+
+get_ice_cream("chocolate")
